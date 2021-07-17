49f44e75622682431dafbaf0d65eefa4dedffd1b
Mêrcy <78019226+mercyxrd@users.noreply.github.com>
2021-07-17 11:04:56 +0300

    Update bankickkoruma.js

--- komutlar/serverguard/bankickkoruma.js
+++ komutlar/serverguard/bankickkoruma.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
 const ayarlar = require('../../ayarlar.json');
 const database = require("../../database/korumalar");
 exports.run = async (receivedMessage, message, args) => {  
-let embed = new MessageEmbed().setColor(ayarlar.embedrenk).setAuthor('Hanzo Guard', message.author.avatarURL({ dynamic: true }));
+let embed = new MessageEmbed().setColor(ayarlar.embedrenk).setAuthor('Cartel Guard', message.author.avatarURL({ dynamic: true }));
 const data = await database.findOne({guildID: message.guild.id});
 if(message.author.id !== message.guild.owner.user.id) return message.channel.send(embed.setDescription(`Bu komutu kullanmak için sunucu sahibi olmalısın.`)).then(x=>x.delete({timeout:5000}))
 if(data.JailBane == false) return message.channel.send(embed.setDescription(`Jail Rolü ayarlanmadığı için işleme devam edemiyorum.\n\`.jailBan ayarla [@Ban]\``)).then(x=>x.delete({timeout:20000}));
