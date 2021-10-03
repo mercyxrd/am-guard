a45897e0b8ae4bca40b01cd0aeddb8e4b060fdb0
Mêrcy <78019226+mercyxrd@users.noreply.github.com>
2021-10-03 20:12:20 +0300

    Delete database directory

--- database/modlog.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require("mongoose");
-const channel = new mongoose.Schema({
-    guildID: String, 
-    channel: String});
-const MessageModel = (module.exports = mongoose.model("Kanal", channel));
--- database/whitelist.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const channel = new mongoose.Schema({
-    WhiteListMembers:  { type: Array, default: [] },
-    WhiteListRoles:  { type: Array, default: [] },
-    WhiteListChannels:  { type: Array, default: [] },
-    ServerID: { type: String, default: '' },
-    FiltredWords:  { type: Array, default: [] },
-});
-const MessageModel = (module.exports = mongoose.model("whitelist", channel));
